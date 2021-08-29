1b425270396d3ba2485ab0735c9fc8a52a3d5b0a
Gabriel Branco <gabriel.branco@odig.net>
2021-08-29 20:08:57 -0300

    23 to 35

---
+++ dom/desafio-02/app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: ''
+    },
+    methods: {
+        exibirAlerta() {
+            alert('Estou te alertando!')
+        },
+        // alterarValor(event) {
+        //     this.valor = event.target.value
+        // }
+
+    }
+})
